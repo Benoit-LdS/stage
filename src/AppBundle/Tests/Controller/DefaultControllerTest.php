c4eaa6f133eb8389f63bd484b8cf95cfb376c89c
Benoit Lubrano <lbeno974@hotmail.fr>
2015-08-02 13:39:27 +0200

    A Single Bundle for the Whole Application
    AppBundle

---
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("CV")')->count() > 0);
+    }
+}
